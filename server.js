57f4815f4586d65a0de8e3affb5786b9caf6876f
Schrese <53787633+Schrese@users.noreply.github.com>
2020-01-15 17:30:00 -0500

    Just setting up what i'll need

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 
+const postsRouter = require('./posts/postRouter.js')
+const userRouter = require('./users/userRouter.js')
+
 const server = express();
+
+server.use(express.json());
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
-
-function logger(req, res, next) {}
+server.use('/api/posts', postsRouter);
+server.use('/api/users', userRouter);
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -2,30 +2,37 @@ const express = require('express');
 
 const router = express.Router();
 
+//POST all users (insert()) MAYBE?????
 router.post('/', (req, res) => {
   // do your magic!
 });
 
+//POST new post by user (insert()) MAYBE????
 router.post('/:id/posts', (req, res) => {
   // do your magic!
 });
 
+//GET all users (get())
 router.get('/', (req, res) => {
   // do your magic!
 });
 
+//GET user by id (getById())
 router.get('/:id', (req, res) => {
   // do your magic!
 });
 
+//GET posts by user id (getUserPosts())
 router.get('/:id/posts', (req, res) => {
   // do your magic!
 });
 
+//DELETE user by id (remove())
 router.delete('/:id', (req, res) => {
   // do your magic!
 });
 
+//PUT edits user info by id (update())
 router.put('/:id', (req, res) => {
   // do your magic!
 });
