5f602b2bf46b2ac18649ac607bf10b993162a52c
Schrese <53787633+Schrese@users.noreply.github.com>
2020-01-15 17:57:08 -0500

    getting all users works, getting user by id works, getting posts by user id works

--- users/userRouter.js
+++ users/userRouter.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
+const Users = require('./userDb.js')
+
 //POST all users (insert()) MAYBE?????
 router.post('/', (req, res) => {
   // do your magic!
@@ -15,16 +17,51 @@ router.post('/:id/posts', (req, res) => {
 //GET all users (get())
 router.get('/', (req, res) => {
   // do your magic!
+  Users.get()
+    .then(users => {
+      res.status(201).json(users)
+    })
+    .catch(err => {
+      console.log('error getting users', err)
+      res.status(500).json({ errorMessage: "The users information could not be retrieved."})
+    })
+
 });
 
 //GET user by id (getById())
 router.get('/:id', (req, res) => {
   // do your magic!
+  const id = req.params.id;
+  Users.getById(id)
+    .then(user => {
+      if (!user) {
+        res.status(404).json({errorMessage: "The post with the specified ID does not exist."})
+      } else {
+        res.status(200).json({user})
+      }
+    })
+    .catch(err => {
+      console.log('error getting specified user', err)
+      res.status(500).json({ errorMessage: 'The user information could not be retrieved. '})
+    })
 });
 
 //GET posts by user id (getUserPosts())
 router.get('/:id/posts', (req, res) => {
   // do your magic!
+  const id = req.params.id;
+  Users.getUserPosts(id)
+    .then(posts => {
+      if (posts.length === 0) {
+        res.status(404).json({ message: "The posts with the specified ID does not exist. "})
+      } else {
+        res.status(200).json({posts})
+      }
+    })
+    .catch(err => {
+      console.log('error getting posts for this user id', err)
+      res.status(500).json({ errorMessage: 'The posts information could not be retrieved.' })
+    })
 });
 
 //DELETE user by id (remove())
