81db2a52fdafaa08c21a73dd25ef49a1cc6b5528
Schrese <53787633+Schrese@users.noreply.github.com>
2020-01-15 19:20:00 -0500

    logger middleware works

--- server.js
+++ server.js
@@ -6,13 +6,20 @@ const userRouter = require('./users/userRouter.js')
 const server = express();
 
 server.use(express.json());
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//custom middleware
+
 server.use('/api/posts', postsRouter);
 server.use('/api/users', userRouter);
+//custom middleware
+function logger (req, res, next) {
+  const {method, originalUrl} = req;
+  console.log(`${method} to ${originalUrl}`);
+  next();
+}
 
 module.exports = server;
